e0fb9d579969a4234ea2063bd9df8354ba6169a8
martamasramon <marta.masramon@gmail.com>
2024-04-20 11:52:36 +0100

    stain normalisation

--- pipeline.py
+++ pipeline.py
@@ -1,9 +1,9 @@
 import os
 import cv2
 import tifffile
-from PIL import Image, ImageOps 
+import numpy as np
     
-def resize_histo_png(folder_in, folder_out, sid, slices, size=600, key=2):      
+def resize_histo_png(folder_in, folder_out, sid, slices, size=600, key=2, factor=100, tif=False):      
         
     for i in range(len(slices)):
         # Read NDPI image
@@ -17,12 +17,18 @@ def resize_histo_png(folder_in, folder_out, sid, slices, size=600, key=2):
         
         # Reshape image
         histo_img   = histo_img.resize((int(size/w*h), size))
+        print(int(size/w*h), size)
         
         # 2x2mm patches for manju
-        # histo_img   = histo_img.resize((int(h/size), int(w/size)))
+        #print(int(h/factor), int(w/factor))
+        #histo_img   = histo_img.resize((int(h/factor), int(w/factor)))
         
-        # Save as png
-        histo_img.save(os.path.join(folder_out, sid + '_' +slices[i]) + '_downscale.png')
+        if tif:
+            # Save as tif
+            tifffile.imsave(os.path.join(folder_out, sid + '_' +slices[i]) + '_high_res.tif', np.array(histo_img))
+        else:
+            # Save as png
+            histo_img.save(os.path.join(folder_out, sid + '_' +slices[i]) + '_high_res.png')
         
     
 def divide_image(path_in, path_out, name, size=512, key=1):
@@ -48,7 +54,34 @@ def divide_image(path_in, path_out, name, size=512, key=1):
             
             if count%1000 == 0:
                 print('Sub-image count: ' + str(count) + ' (' + str(count/total*100) + '%)')             
-  
+
+def divide_image_reshape(path_in, path_out, name, size=512, key=1):
+    print(name)
+    size_big = size*2
+    histo    = tifffile.imread(path_in + name + '.ndpi', key=key)
+    
+    print('Shape:',histo.shape)
+    [w,h,_] = histo.shape
+    
+    count   = 0
+    total   = int(w/size_big) * int(h/size_big)
+    print('Number of tiles:',total)
+    
+    if not os.path.exists(path_out):
+        os.mkdir(path_out)   
+     
+          
+    for i in range(int(w/size_big)):
+        for j in range(int(h/size_big)):
+            img = histo[i*size_big:(i+1)*size_big, j*size_big:(j+1)*size_big, :] 
+            img = Image.fromarray(img)
+            img = img.resize((size, size))
+            img.save(path_out + name + '_' + str(size) + '_' + str(count) + '.png')
+            count += 1
+            
+            if count%1000 == 0:
+                print('Sub-image count: ' + str(count) + ' (' + str(count/total*100) + '%)')   
+                
 slices = {
     #'HMU_033_JS': {'key': 2, 'nums': ['A3']},
     #'HMU_038_JC': {'key': 2, 'nums': ['A2']},
@@ -61,30 +94,42 @@ slices = {
     #'HMU_069_NS': {'key': 2, 'nums': ['A4']}
     #'HMU_076_RV': {'key': 2, 'nums': ['A4']},
     #'HMU_077_MW': {'key': 2, 'nums': ['A3']},
-    #'HMU_084_AJ': {'key': 1, 'nums': ['A2','A3']},
+    'HMU_082_PS': {'key': 2, 'nums':['A3']}
+    #'HMU_084_AJ': {'key': 1, 'nums': ['A2']}, #,'A3']},
     #'HMU_087_FM': {'key': 1, 'nums': ['A3']},
     #'HMU_094_RB': {'key': 1, 'nums': ['A1','A3']},
     #'HMU_099_DL': {'key': 1, 'nums': ['A4','A7']},
+    #'HMU_113_MT': {'key': 1, 'nums': ['A2']}, 
     #'HMU_121_CN': {'key': 1, 'nums': ['A1']}
-    'HMU_180_KF': {'key':1, 'nums':['A3']}
+    #'HMU_176_IJ': {'key': 2, 'nums': ['A3']}, 
+    #'HMU_180_KF': {'key':2, 'nums':['A3','A4','A5']},
+    #'HMU_198_JL': {'key': 0, 'nums': ['A2']},
+    #'HMU_201_MB': {'key': 2, 'nums': ['A3']} 
+    # 'HMU_242_JD': {'key': 0, 'nums': ['A5']}, # too dark...?
+    # 'HMU_245_DC': {'key': 0, 'nums': ['A7']},
+    # 'HMU_256_DB': {'key': 0, 'nums': ['A6']},
+    # 'HMU_258_JK': {'key': 0, 'nums': ['A6']},
+    # 'HMU_265_JM': {'key': 0, 'nums': ['A7']}
 }
 
 def main(): 
     # Choose pipeline options
     RESIZE       = True
-    MAKE_PATCHES = False
+    MAKE_PATCHES = True
     
     folder_in  = './Whole images/'  
     folder_out = './Downsampled/'  
     
     for sid in slices:
         if RESIZE:
-            resize_histo_png(folder_in, folder_out, sid, slices[sid]['nums'])
+            resize_histo_png(folder_in, folder_out, sid, slices[sid]['nums'],key=slices[sid]['key'])
+            # for manju, size =30
         
         if MAKE_PATCHES:
             for i in range(len(slices[sid]['nums'])):
                 name = sid + '_' + slices[sid]['nums'][i]
-                divide_image(folder_in,  'Patches/' + name + '/' , name, 512, key=slices[sid]['key']) 
-                #for manju, size=4406
+                divide_image(folder_in,  'Patches/' + name + '/' , name, 512, key=slices[sid]['key'])   #for manju, size=4406
+                # For images with key=0:
+                # divide_image_reshape(folder_in,  'Patches/' + name + '/' , name, 512, key=slices[sid]['key'])
             
 main()  
--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
-#$ -l tmem=80G,h_vmem=80G
+#$ -l tmem=64G,h_vmem=64G
 #$ -l h_rt=10:00:00
 
 #$ -S /bin/bash
 #$ -j y
-#$ -N Patches
+#$ -N Test
 #$ -V
 #$ -wd /cluster/project7/ProsRegNet_CellCount/Cellcount
 
@@ -14,7 +14,6 @@ date
 export PATH=/share/apps/python-3.8.5-shared/bin:$PATH
 export LD_LIBRARY_PATH=/share/apps/python-3.8.5-shared/lib:$LD_LIBRARY_PATH
 
-python3 pipeline.py
+python3 simple_SN.py
 
-date
-
+dat
